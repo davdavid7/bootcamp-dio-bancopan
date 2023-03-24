4d867e3c815e07f3117f7acce291fb39d3b17c8f
davdavid7 <davfernando7@gmail.com>
2023-03-24 11:53:28 -0300

    código da aula de tipos e variáveis

--- codigos-das-aulas/src/edu/david/sintaxejava/TiposVariaveis.java
+++ codigos-das-aulas/src/edu/david/sintaxejava/TiposVariaveis.java
@@ -1,5 +1,15 @@
 package edu.david.sintaxejava;
 
 public class TiposVariaveis {
-    
+    public static void main (String [] args){
+        int inteiro = 2000;
+        long longo = 122323423534535l;
+        float deciaml = 2.0f;
+        double decimalLongo = 56.1234678;
+        char caractere = 'm';
+        String sequenciaCaractere = "David";
+        boolean verdade = true;
+        boolean falso = false;
+
+    }
 }
